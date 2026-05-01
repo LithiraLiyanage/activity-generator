7d9bf8e1723db44dbf573c4a8e2e311515e43083
Lithira Liyanage <lithiraliyanage666@gmail.com>
2026-05-01 12:00:00 +0530

    commit 1

--- generate.bat
+++ generate.bat
@@ -4,11 +4,12 @@ setlocal EnableDelayedExpansion
 git config user.name "Lithira Liyanage"
 git config user.email "lithiraliyanage666@gmail.com"
 
-for /L %%i in (1,1,30) do (
-    echo commit %%i>>file.txt
-    git add .
-    set /a DAY=!RANDOM! %% 28 + 1
-    git commit --date="2026-05-!DAY!T12:00:00"
+for /L %%d in (1,1,28) do (
+    for /L %%i in (1,1,5) do (
+        echo commit %%i>>file.txt
+        git add .
+        git commit --date="2026-05-%%dT12:00:00" -m "commit %%i"
+    )
 )
 
 pause
